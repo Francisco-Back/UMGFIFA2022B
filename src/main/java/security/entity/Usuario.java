3b3ee6b2a716d5619abc1c3d936257f19f142d0d
MarcodPG98 <mg32834@gmail.com>
2022-09-11 20:10:19 -0600

    Package_and_Usuario

---
+++ src/main/java/security/entity/Usuario.java
@@ -0,0 +1,100 @@
+package security.entity;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id_usuario;
+    @NotNull
+    private String nombre;
+    @NotNull
+    @Column(unique = true)
+    private String nombreUsuario;
+    @NotNull
+    private String email;
+    @NotNull
+    private String password;
+    @NotNull
+    private String avatar;
+    @NotNull
+    @ManyToMany
+    @JoinTable(name = "usuarioRol", joinColumns = @JoinColumn(name = "id_usuario"),
+    inverseJoinColumns = @JoinColumn(name = "rol_id"))
+    private Set<Rol> roles = new HashSet<>();
+
+    public Usuario() {
+    }
+
+    public Usuario(@NotNull String nombre, @NotNull  String nombreUsuario,
+                   @NotNull String email, @NotNull  String password,
+                   @NotNull String avatar) {
+        this.nombre = nombre;
+        this.nombreUsuario = nombreUsuario;
+        this.email = email;
+        this.password = password;
+        this.avatar = avatar;
+    }
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public Set<Rol> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Rol> roles) {
+        this.roles = roles;
+    }
+}
