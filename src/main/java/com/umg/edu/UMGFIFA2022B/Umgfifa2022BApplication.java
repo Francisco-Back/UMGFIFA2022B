1bf068e6cb18861b74b4a08185980eeb82012b0c
annabellaaroche <110144837+annabellaaroche@users.noreply.github.com>
2022-09-15 23:01:54 -0600

    pruebaCrossGeneral
    pruebaCrossGeneral

--- src/main/java/com/umg/edu/UMGFIFA2022B/Umgfifa2022BApplication.java
+++ src/main/java/com/umg/edu/UMGFIFA2022B/Umgfifa2022BApplication.java
@@ -2,7 +2,10 @@ package com.umg.edu.UMGFIFA2022B;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableWebMvc
@@ -10,6 +13,16 @@ public class Umgfifa2022BApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Umgfifa2022BApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("/**").allowedMethods("*").allowedHeaders("*");
+			}
+		};
 	}
 
 }
