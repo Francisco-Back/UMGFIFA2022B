261dd8da6a51fce79b4bdde7c33c3c93540ef501
annabellaaroche <110144837+annabellaaroche@users.noreply.github.com>
2022-09-15 22:29:39 -0600

    CrossOrigin
    Agregue Cross Origin al principio

--- src/main/java/com/umg/edu/UMGFIFA2022B/controller/UserController.java
+++ src/main/java/com/umg/edu/UMGFIFA2022B/controller/UserController.java
@@ -3,6 +3,7 @@ package com.umg.edu.UMGFIFA2022B.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import com.umg.edu.UMGFIFA2022B.services.dto.UserlnDTO;
 
 @RestController
 @RequestMapping("/api/User")
+@CrossOrigin(origins = "*")
 public class UserController {
 	@Autowired	
 	private final UserService userservice;
