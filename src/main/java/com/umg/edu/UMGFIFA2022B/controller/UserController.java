4e68aed4583d40dff33c7d63422678756f7cff7d
annabellaaroche <110144837+annabellaaroche@users.noreply.github.com>
2022-09-17 15:41:18 -0600

    Revert "Change User route"
    This reverts commit 6681fd3cc99d706163449d2f8532fef4df2c6a29.

--- src/main/java/com/umg/edu/UMGFIFA2022B/controller/UserController.java
+++ src/main/java/com/umg/edu/UMGFIFA2022B/controller/UserController.java
@@ -16,6 +16,7 @@ import com.umg.edu.UMGFIFA2022B.services.dto.UserlnDTO;
 
 @RestController
 @RequestMapping("/User")
+@CrossOrigin(origins = "*")
 public class UserController {
 	@Autowired	
 	private final UserService userservice;
@@ -24,7 +25,6 @@ public class UserController {
 	public UserController(UserService userservice) {
 		this.userservice = userservice;
 	}
-	@CrossOrigin(origins = "*")
 	@PostMapping
 	public ResponseEntity<?> createUser(@RequestBody  UserlnDTO userlnDTO  ) {
 		return new ResponseEntity<>(userservice.createUser(userlnDTO),HttpStatus.CREATED);
